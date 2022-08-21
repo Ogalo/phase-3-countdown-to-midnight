b430434eade81ec8c73cac1cc065946ce0659d30
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-22 02:05:18 +0300

    commit tree 61490bec9a295aaed7402795105be16d411c3cb3

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown (num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+
+
+  end
+  "HAPPY NEW YEAR!"
+
+
+end
+
+puts countdown(5)
+
+def countdown_with_sleep (number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep (1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+
 end
